252c620aff20f05645a532844d120afb38b69a49
shj <josh2kv@gmail.com>
2025-07-26 01:39:35 +0900

    feat(deps): add new dependencies and update config

--- src/lib/api/api.ts
+++ src/lib/api/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { env } from '../../config/env';
+import { env } from '@/config/env';
 
 export const api = axios.create({
   baseURL: `${env.VITE_API_URL}${env.VITE_API_URL_PREFIX}`,
